0d325fb02ae6cc3f200e50e00fc3c45b3b97ea21
utkarsh bhargava <bhargavautkarsh074@gmail.com>
2024-02-15 00:23:19 +0530

    deployed

--- src/app/actions/getCommentByVideoId.ts
+++ src/app/actions/getCommentByVideoId.ts
@@ -1,31 +1,36 @@
 import { Comment,Channel } from "@prisma/client";
 import prisma from "../../../vendor/db";
 
-interface GetCommentByVideoIdParams{
-    videoId?:String
+interface GetCommentsByVideoIdParams {
+  videoId?: string;
 }
-export default async function getCommentByVideoId(params:GetCommentByVideoIdParams)
-:Promise<(Comment & {channel:Channel})[] | null>{
-    try{
-     const {videoId}=params;
-     const query:any={}
-     if(videoId){
-        query.videoId=videoId;
-     }
-     const comments=await prisma.comment.findMany({
-     where:query,
-     include:{
-        channel:true
-     },
-     orderBy:[
-     {
-        createdAt:"desc"// descending order
-     }
-    ],
-     })
-     return comments;
+
+export default async function getCommentsByVideoId(
+  params: GetCommentsByVideoIdParams
+): Promise<(Comment & { channel: Channel })[] | null> {
+  try {
+    const { videoId } = params;
+
+    const query: any = {};
+
+    if (videoId) {
+      query.videoId = videoId;
     }
-    catch(error:any){
+
+    const comments = await prisma.comment.findMany({
+      where: query,
+      include: {
+        channel: true,
+      },
+      orderBy: [
+        {
+          createdAt: "desc",
+        },
+      ],
+    });
+
+    return comments;
+  } catch (error: any) {
     throw new Error(error);
-    }
+  }
 }
